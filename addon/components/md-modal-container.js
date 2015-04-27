3243287f8593ce895c264c9ff9d3ef45d820ebd9
Stefan Gasser <stefangasser.info@gmail.com>
2015-04-27 21:28:07 +0200

    Publishing to github pages

--- addon/components/md-modal-container.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-import layout from '../templates/components/md-modal-container';
-
-export default Ember.Component.extend({
-  modalContainerId: 'materialize-modal-root-element',
-  layout: layout
-});
